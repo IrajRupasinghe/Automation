8eb0d4007475d3f6fda69f5c9193e6cf66cff3d3
IrajRupasinghe <irajrupasinghe@gmail.com>
2025-12-22 02:30:19 +0000

    Playwright_Framework_verifyURL

---
+++ POM-playwright/base/playwright-dev-page.ts
@@ -0,0 +1,42 @@
+import { expect, type Locator, type Page } from '@playwright/test';
+import datafile from "../data/data.json";
+
+export class PlaywrightDevPage {
+  readonly page: Page;
+  readonly getStartedLink: Locator;
+  readonly gettingStartedHeader: Locator;
+  readonly pomLink: Locator;
+  readonly cUrl: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.getStartedLink = page.locator('a', { hasText: 'Get started' });
+    this.gettingStartedHeader = page.locator('h1', { hasText: 'Installation' });
+    this.cUrl = page.getByText('How to install Playwright');
+    this.pomLink = page.locator('li', {hasText: 'Guides',}).locator('a', {hasText: 'Page Object Models',});
+  }
+
+  async goto() {
+    //await this.page.goto('https://playwright.dev');
+    await this.page.goto(datafile.url);
+  }
+
+  async getStarted() {
+    await this.getStartedLink.first().click();
+    await expect(this.gettingStartedHeader).toBeVisible();
+  }
+
+  async clickUrl() {
+    await this.cUrl.click();
+  }
+
+  async pageObjectModel() {
+    await this.getStarted();
+    await this.pomLink.click();
+  }
+
+  // to close the open browser
+  async closebrowser(){
+    await this.page.close();
+  }
+}
---
+++ POM-playwright/tests/regression-test-cases.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { PlaywrightDevPage } from '../base/playwright-dev-page';
+
+test('getting started should contain table of contents, tes01', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.getStarted();
+  await playwrightDev.clickUrl();
+
+const currentUrl = page.url();
+const expectedUrl = 'https://playwright.dev/docs/intro#installing-playwright';
+
+  if (currentUrl === expectedUrl) { console.log('Page redirected to the correct site.');} 
+  else { console.log('Page not redirected ERORR!');}
+  await playwrightDev.closebrowser();
+});
+
+test('should show Page Object Model article, test02', async ({ page }) => {
+  const playwrightDev = new PlaywrightDevPage(page);
+  await playwrightDev.goto();
+  await playwrightDev.pageObjectModel();
+  await expect(page.locator('article')).toContainText('Large test suites can be structured to optimize ease of authoring and maintenance.');
+  await playwrightDev.closebrowser();
+});
